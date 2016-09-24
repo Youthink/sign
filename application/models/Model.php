e885ebc201e8141cdb590ccfc006d18026f0ab08
Youthink <hi@hufangyun.com>
2016-09-25 07:39:12 +0800

    数据库数据每日签到的唯一值

--- application/models/Model.php
+++ application/models/Model.php
@@ -10,15 +10,23 @@ class Model extends CI_Model
 
       public function M_addsign()
     {
+      
+      $num = $this->input->post('num');
+      $sql = "select * from sign where num =? and date = ?";
+      $query = $this->db->query($sql,array($num,date("Y-m-d")))
+                    ->result_array();
+      if(!$query){
 
         $data = array(
 
             'username' => $this->input->post('username'),
-            'num' => $this->input->post('num'),
+            'num' => $num,
         );
-        $this->db->set('date',date('Y-m-d'));
-        $this->db->set('time',date('H:s:i'));
+        $this->db->set('date',date("Y-m-d"));
+        $this->db->set('time',date("H:i:s"));
         return $this->db->insert('sign', $data);
+
+      }
 
     }
 
@@ -29,8 +37,9 @@ class Model extends CI_Model
               /*$this->db->order_by('date')
                    ->where('date',date('Y-m-d'));
               $query = $this->db->get('sign');*/
-              $sql = "select *,count(distinct username) from sign where date = ? group by username order by time";
-              $query = $this->db->query($sql,array(date('Y-m-d')));
+
+              $sql = "select *,count(distinct num) from sign where date = ? group by num order by time";
+              $query = $this->db->query($sql,array(date("Y-m-d")));
               return $query->result_array();
         }else{
         
